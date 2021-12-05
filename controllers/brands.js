af6d66449fb939a7b0bd36e210a47a8f29d67a3d
BENJMILA, Abdelhalim (EXT) <abdelhalim.benjmila.ext@stago.com>
2021-12-05 23:36:45 +0100

    add brands

--- controllers/brands.js
+++ controllers/brands.js
@@ -1,10 +1,32 @@
 let Brand = require('../models/brand');
 
-async function allBrands(req, res, next) {
+async function allBrands() {
     const brands = await Brand.find({});
-    return res.json(brands);
+    return brands;
+}
+
+async function addBrands(req, res, next) {
+    try {
+        let logo = '';
+        if (req.file) {
+            console.log(req.file)
+            logo = `/uploads/${req.file.filename}`;
+        }
+        const {
+            name
+        } = req.body;
+
+        const brand = new Brand({name, logo});
+        await brand.save();
+
+        return res.redirect('/brands');
+    } catch (e) {
+        console.error(e);
+        return res.render('brands/add.twig', {error: 'error or exist brand'});
+    }
 }
 
 module.exports = {
     allBrands,
+    addBrands,
 }
--- routes/brands.js
+++ routes/brands.js
@@ -2,9 +2,32 @@ const express = require('express');
 const router = express.Router();
 let Brand = require('../models/brand');
 const brandController = require('../controllers/brands')
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './public/uploads/');
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-';
+        cb(null, uniqueSuffix + file.originalname);
+    }
+})
+
+const upload = multer({storage: storage})
+
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-    return brandController.allBrands(req, res, next);
+router.get('/', async function (req, res, next) {
+    let brands = await brandController.allBrands();
+    res.render('brands/index', {brands});
+});
+router.get('/add', function (req, res, next) {
+    // return brandController.allBrands(req, res, next);
+    res.render('brands/add');
+});
+
+router.post('/add', upload.single('logo'), async function (req, res, next) {
+    return await brandController.addBrands(req, res, next);
 });
 
 module.exports = router;
