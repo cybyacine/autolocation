a4d152199655b6bc7d2f9e6236001c4e73660ed8
cybyacine <yacine.benouirane@esprit.tn>
2021-12-07 17:27:51 +0100

    suite commit ybenouirane

--- routes/spare-parts.js
+++ routes/spare-parts.js
@@ -1,10 +1,34 @@
 const express = require('express');
 let router = express.Router();
 let SparePart = require('../models/spare-part');
+const SparePartController = require('../controllers/spare-parts');
+const multer = require('multer');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './public/uploads/');
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-';
+        cb(null, uniqueSuffix + file.originalname);
+    }
+})
+
+const upload = multer({storage: storage})
+
+/* GET spare-parts listing. */
+router.get('/', async function (req, res, next) {
+  let spareParts = await SparePartController.allSpareParts();
+  res.render('spare-parts/index', {spareParts});
 });
+
+router.get('/add', function (req, res, next) {
+  res.render('spare-parts/add');
+});
+
+router.post('/add', upload.single('logo'), async function (req, res, next) {
+    return await SparePartController.addSpareParts(req, res, next);
+});
+
 
 module.exports = router;
